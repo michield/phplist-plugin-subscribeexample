823b30be7ac7c42db4d676fac58ae13789b4f0ce
Michiel Dethmers <git@phplist.com>
2013-06-10 16:41:39 -0300

    check var existence

--- plugins/subscribepageexample.php
+++ plugins/subscribepageexample.php
@@ -32,8 +32,12 @@ class subscribepageexample extends phplistPlugin {
 
   function processSubscribePageEdit($id) {
     
-    $myValue = $_POST['myField'];
-    $myValue = preg_replace('/[^\w -]+/','',$myValue);
+    if (!empty($_POST['myField'])) {
+      $myValue = $_POST['myField'];
+      $myValue = preg_replace('/[^\w -]+/','',$myValue);
+    } else {
+      $myValue = 'Default value';
+    }
     
     Sql_Query(sprintf('replace into %s (id,name,data) values(%d,"myField","%s")',
        $GLOBALS['tables']["subscribepage_data"],$id,sql_escape($myValue)));
@@ -41,7 +45,7 @@ class subscribepageexample extends phplistPlugin {
 
   
   function displaySubscriptionChoice($pageData, $userID = 0) {
-    if ($pageData['myField'] == 'check') {
+    if (!empty($pageData['myField']) && $pageData['myField'] == 'check') {
       return s('Please do not enter anything in this field').' '.'<input type="text" name="myValidationCheck" value="" />';
     } else {
       return '<!-- nothing to do -->';
@@ -49,7 +53,7 @@ class subscribepageexample extends phplistPlugin {
   }
  
   function validateSubscriptionPage($pageData) {
-    if ($pageData['myField'] == 'check') {
+    if (!empty($pageData['myField']) && $pageData['myField'] == 'check') {
       if (!empty($_POST['myValidationCheck'])) {
         return s('Sorry, you did enter something, we requested not to do so');
       }
